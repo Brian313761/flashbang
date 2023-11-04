f962d769db4114a5a4f4bdda582a497527e01a0f
Brian313761 <140124219+Brian313761@users.noreply.github.com>
2023-11-03 22:23:26 -0400

    Add files via upload

---
+++ flashbang/src/flashbang/flashbang2.java
@@ -0,0 +1,61 @@
+package flashbang;
+
+/*import java.util.Scanner;*/
+
+public class flashbang2 {
+
+	public static int area(int length, int width) {
+		return length*width;
+	}
+	
+	public static void main(String[] args) {
+	
+		System.out.println(area(5,5));
+	//Scanner num = new Scanner(System.in);
+
+/*System.out.print("insert a first number: ");
+int firstNumber = num.nextInt();
+
+System.out.print("insert a second number: ");
+int secondNumber = num.nextInt();
+
+int sum = firstNumber+secondNumber;
+int car = firstNumber-secondNumber;
+int non = firstNumber*secondNumber;
+int the = firstNumber/secondNumber;
+int mod = firstNumber % secondNumber;
+
+System.out.println(sum);
+System.out.println(car);
+System.out.println(non);
+System.out.println(the);
+System.out.println(mod);*/
+	
+	/*System.out.println ((25.5*3.5-3.5*3.5)/(40.5-4.5);*/
+		
+		/*Scanner swap = new Scanner(System.in);
+		
+		System.out.print("Frist number: ");
+		int num1= swap.nextInt();
+		
+		System.out.print("second number: ");
+		int num2= swap.nextInt();
+		
+		num1= num1+num2;
+		num2= num1-num2;
+		num1= num1-num2;
+		
+		System.out.println("after swapping: ");
+		System.out.println(num1);
+		System.out.println(num2);*/
+		
+		/*Scanner calculate = new Scanner(System.in);
+		
+		System.out.print("Enter the year: ");
+	int base= calculate.nextInt();
+	
+	if((base%4==0 && base%100!=0) || (base%400==0)) {
+		System.out.println(base + "is not a leap year ");
+	}*/
+}
+}
